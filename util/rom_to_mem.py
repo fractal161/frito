5047d30bcf5a46b3ed0fdcda295d9df0db3eac15
fractal161 <628snapdragon@gmail.com>
2023-11-08 03:07:20 -0500

    script for loading a rom

---
+++ util/rom_to_mem.py
@@ -0,0 +1,18 @@
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print('not enough arguments')
+    else:
+        rom = b''
+        with open(sys.argv[1], 'rb') as f:
+            rom = f.read()
+        byte_list = [b.to_bytes(1, sys.byteorder).hex() for b in rom]
+        # pad with 0x200 zeroes
+        byte_list = ['00' for _ in range(0x200)] + byte_list
+        # pad to 0x1000 total length
+        byte_list.extend('00' for _ in range(0x1000 - len(byte_list) + 0x100 + 18))
+        # pc
+        byte_list.extend(('02', '00'))
+        with open(sys.argv[2], 'w') as f:
+            f.write(' '.join(byte_list))
